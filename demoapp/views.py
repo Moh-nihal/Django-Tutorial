f172519447a1635eb7f65bf40eb8c304b088c442
Moh-nihal <mohammednihal8771@gmail.com>
2025-02-17 17:59:27 +0530

    after login and signup

--- demoapp/views.py
+++ demoapp/views.py
@@ -9,28 +9,54 @@ def index(request):
     return render(request,'index.html',{"services":service})
 
 def register(request):
-    if request.method=='POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        password2 = request.POST['password2']
+    if request.method == 'POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        password2=request.POST['password2']
 
         if password == password2:
-            if User.objects.filter(email = email).exists():
-                messages.info(request,'Email already existis')
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'The Email already exists')
                 return redirect ('register')
-            elif User.objects.filter(username = username).exists():
-                messages.info(request,'Username Already Exists')
-                return redirect('register')
+
+            elif User.objects.filter(username =username).exists():
+                messages.info(request,'The Username already exists')
+                return redirect ('register')
+            
             else:
                 user=User.objects.create_user(username=username,email=email,password=password)
                 user.save()
-                return redirect('login')
+                return redirect ('login')
         else:
-            messages.info(request,'Password does not match')
-            return redirect('register')
+            messages.info(request,'The passwords doses not match')
+            return redirect(request,'register')
+
     else:
-        return render(request,'register.html')
+        return render(request,'login.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'Credentials Invalid')
+            return redirect('login')
+    
+    return render(request,'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+
 
 def nextpage(request):
     # words = request.POST['Value']
